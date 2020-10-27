e7835ba602b6365c7246775e662924653a868e39
themak01 <37306115+themak01@users.noreply.github.com>
2020-10-27 11:16:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//My routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const profileRoutes = require("./routes/profile");
+const postRoutes = require("./routes/post")
+//DB Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", profileRoutes);
+app.use("/api", postRoutes)
+
+//PORT
+const port = process.env.PORT || 8000;
+
+//Starting a server
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
